3b84b58a73f42bf024e8385385779b1a26541bfb
raphaelroques <raphaelroques@gmail.com>
2020-09-28 12:20:55 -0400

    Updates

---
+++ precache-manifest.8dc1a71cd719a4dd494be0fdf76d97a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7942abfaf7b202137c649892538fe4",
+    "url": "/raphaelroques/deploy-test/index.html"
+  },
+  {
+    "revision": "be0353c81239f32a90d6",
+    "url": "/raphaelroques/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fcb30bad4463ba851944",
+    "url": "/raphaelroques/deploy-test/static/js/2.a3087b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raphaelroques/deploy-test/static/js/2.a3087b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0353c81239f32a90d6",
+    "url": "/raphaelroques/deploy-test/static/js/main.130d1bf3.chunk.js"
+  },
+  {
+    "revision": "ff0268d8e0eb110a6b69",
+    "url": "/raphaelroques/deploy-test/static/js/runtime-main.05293cee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/raphaelroques/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
